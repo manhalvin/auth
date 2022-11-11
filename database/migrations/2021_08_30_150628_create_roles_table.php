ad1614cd5482ca093b0c44996e30b54c8d4d84c4
manhalvin <quocmanh.tran@gumiviet.com>
2022-11-11 17:07:13 +0700

    Hotfix Design Auth

--- database/migrations/2021_08_30_150628_create_roles_table.php
+++ database/migrations/2021_08_30_150628_create_roles_table.php
@@ -14,7 +14,7 @@ class CreateRolesTable extends Migration {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description')->nullable();
+            $table->text('content')->nullable();
             $table->timestamps();
         });
     }
